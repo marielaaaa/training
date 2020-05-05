28de9693206169f564cbfaa0179add163e263b4f
marielaaaa <64827376+marielaaaa@users.noreply.github.com>
2020-05-05 14:37:39 +0800

    Update HelloUniverse.java

--- src/basics/HelloUniverse.java
+++ src/basics/HelloUniverse.java
@@ -4,6 +4,7 @@ public class HelloUniverse {
 	public static void main(String[] args) {
 		System.out.println("What's up Universe?");
 		System.out.println("What's up Github?");
+		System.out.println("This is from Github!");
 		int a = 20;
 		 if (a >= 20){
 			 System.out.println("a");
